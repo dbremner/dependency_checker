548e42f1f77ce464a581a53bed2307d10f83bfc9
David Bremner <dbremner@gmail.com>
2018-05-28 16:05:26 -0700

    use pattern matching and enhanced switch construct

--- DependencyChecker/CheckEvaluators/ExpressionCheckEvaluator.cs
+++ DependencyChecker/CheckEvaluators/ExpressionCheckEvaluator.cs
@@ -34,26 +34,18 @@ namespace DependencyChecker.CheckEvaluators
 
         private bool Interpret(PropExpression e, IEvaluationContext context)
         {
-            if (e is PropIdentifier)
+            switch (e)
             {
-                var pi = (PropIdentifier)e;
-                return context.Evaluate(context[pi.Name]);
+                case PropIdentifier pi:
+                    return context.Evaluate(context[pi.Name]);
+                case PropConjunction pc:
+                    return this.Interpret(pc.Left, context) && this.Interpret(pc.Right, context);
+                case PropDisjunction pd:
+                    return this.Interpret(pd.Left, context) || this.Interpret(pd.Right, context);
+                case PropNegation pn:
+                    return !this.Interpret(pn.Inner, context);
             }
-            if (e is PropConjunction)
-            {
-                var pc = (PropConjunction)e;
-                return this.Interpret(pc.Left, context) && this.Interpret(pc.Right, context);
-            }
-            if (e is PropDisjunction)
-            {
-                var pd = (PropDisjunction)e;
-                return this.Interpret(pd.Left, context) || this.Interpret(pd.Right, context);
-            }
-            if (e is PropNegation)
-            {
-                var pn = (PropNegation)e;
-                return !this.Interpret(pn.Inner, context);
-            }
+
             throw new NotImplementedException();
         }
     }
